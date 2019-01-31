3727ca0510b3b5b35f51798043b34aac2cc43d85
Andrea Bacciu <36055796+andreabac3@users.noreply.github.com>
2019-01-31 09:23:19 +0100

    Update Problem4.py

--- Problem 4 - Stripe/Problem4.py
+++ Problem 4 - Stripe/Problem4.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 def solve(arraylist):
-    min = 10
+    min1 = 10
     min2 = 10
     for elem in arraylist:
         if elem <= 0:
             continue
-        if elem < min:
-            min2 = min
-            min = elem
+        if elem < min1:
+            min2 = min1
+            min1 = elem
         elif elem < min2:
             min2 = elem
-    if min2 - min == 1:
+    if min2 - min1 == 1:
         return min2 + 1
-    elif min2 - min > 1:
-        return min + 1
+    elif min2 - min1 > 1:
+        return min1 + 1
 
 if __name__ == "__main__":
     list1 = [3, 4, -1, 1]
