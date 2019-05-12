c9d236190c2dfa165c43786b93fee79d6491ae28
Andrea Bacciu <36055796+andreabac3@users.noreply.github.com>
2019-05-13 00:11:25 +0200

    Update solve.rs

--- problem109 - Cisco/solve.rs
+++ problem109 - Cisco/solve.rs
@@ -1,15 +1,19 @@
 
 fn main() {
-    println!("{}",solve("11100010".to_string()) == "11010001");
+    let mut input_string:String = "11100010".to_string();
+    solve(&mut input_string);
+    println!("{}",input_string == "11010001");
     // assertResult = '11010001'
-    println!("{}",solve("10101010".to_string()) == "01010101");
+    input_string = "10101010".to_string();
+    solve(&mut input_string);
+    println!("{}", input_string == "01010101");
     // assertResult = '01010101'
 }
 
-fn solve(mut input_string:String ) -> String {
+fn solve(input_string: &mut String ) {
     let mut i:usize = 0;
-    let lenght:usize = input_string.len();
-    while i < (lenght - 1) {
+    let lenght:usize = input_string.len() - 1;
+    while i < (lenght) {
         if input_string.chars().nth(i).unwrap() == '1' && input_string.chars().nth(i+1).unwrap() == '0'{
             input_string.replace_range(i..i+2, "01");
         }
@@ -18,5 +22,4 @@ fn solve(mut input_string:String ) -> String {
         }
         i = i + 2;
     }
-    return input_string;
 }
